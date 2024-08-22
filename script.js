dab7fc2a711e5df1283845ad2d4eee8905d715ba
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-08-22 20:57:24 +0500

    third commit

--- script.js
+++ script.js
@@ -1,29 +1,89 @@
-const musicContainer =    document.querySelector('.music-container');
-const playBtn =           document.querySelector('#play');
-const prevBtn =           document.querySelector('#prev');    
-const nextBtn =           document.querySelector('#next');
-const audio =             document.querySelector('#audio');
-const progress =          document.querySelector('.progress');
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
-const title =             document.querySelector('.#title');
-const cover =             document.querySelector('#cover');
-
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
 
 // Song titles
-const songs = ['Tu hai kahan', 'Skechers', 'Wishes', 'Mi amor', 'Excuses'];
+const songs = [
+    { title: 'excuses', extension: 'jpg' },
+    { title: 'love nwantiti', extension: 'jpeg' },
+    { title: 'mi amor', extension: 'jpeg' },
+    { title: 'skechers', extension: 'jpeg' },
+    { title: 'tu hai kahan', extension: 'jpeg' },
+    { title: 'wishes', extension: 'jpg' }
+];
 
 // keep track of songs i
-const songIndex = 4;
+let songIndex = 4;
 
-// Initially loads songs into DOM
-loadSongs( songs[songIndex])
+// Initially load songs into DOM
+loadSong(songs[songIndex]);
 
-//update songs details
-function loadSongs(song) {
-    title.innerText = song;
-    audio.src = `Music/${song}.mp3`;
-    cover.src = `Images/${song}.jpg`;
+// Update song details
+function loadSong(song) {
+    title.innerText = song.title;
+    audio.src = `Music/${song.title}.mp3`; 
+    cover.src = `Images/${song.title}.${song.extension}`;
+    audio.load(); 
 }
 
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
 
+    audio.play();
+}
 
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+function prevSong() {
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function updateProgress(e) {
+
+}
+
+// Event Listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+// Change songs events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress)
